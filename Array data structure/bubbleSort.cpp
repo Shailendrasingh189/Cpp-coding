30c69489e551eb217248a75404a7191cb96ed0ba
shailendrasingh189 <shailendrasinghpanwar189@gmail.com>
2025-04-24 22:43:28 +0530

    bubble sort algorithm

---
+++ Array data structure/bubbleSort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+// bubble sort algorithm
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        bool isSwap = false;
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                isSwap = true;
+            }
+        }
+        if(!isSwap) {
+            return;
+        }
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = 7;
+
+    bubbleSort(arr, n);
+    printArray(arr, n);
+
+    return 0;
+}

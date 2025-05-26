7bb0ecd7ae11ac09fce6e42a5f726bc3717d7858
shailendrasingh189 <shailendrasinghpanwar189@gmail.com>
2025-05-26 13:24:23 +0530

    Middle of linked list

---
+++ LinkedList/middleOfLinkedList.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{ // class to create a node
+public:
+    int data;
+    Node *next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List
+{
+    Node *head;
+    Node *tail;
+
+public:
+    List()
+    {
+        head = tail = NULL;
+    }
+
+    void push_back(int val)
+    {
+        Node *newNode = new Node(val);
+
+        if (head == NULL)
+        {
+            head = tail = newNode;
+            return;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void findMiddle()
+    {
+        Node *slow = head;
+        Node *fast = head;
+
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        cout << "Middle node : " << slow << " and data value" << slow->data << endl; // middle of a linked list
+    }
+
+    void printLL()
+    {
+        Node *temp = head;
+
+        while (temp != NULL)
+        {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main()
+{
+    List ll;
+
+    ll.push_back(1);
+    ll.push_back(2);
+    ll.push_back(3);
+    ll.push_back(4);
+    ll.push_back(5);
+
+    ll.printLL();
+
+    ll.findMiddle();
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -75,6 +75,7 @@ Implementation of strings and **solutions to various LeetCode problems with diff
 Implementation of Linked List and solutions to various Leetcode problems with different approaches.
 
 - [ ] **Single Linked-List** - program for linked list implementation 
+- [ ] **Middle of Linked List** - leetcode 876
 - [ ] **Reverse Linked List** - problem 206 in leetcode
 
 #### 🔹 [Recursion](./Recursion/)  
